e487e4f44b8ee8a27a37637f91c7bcbd1523b997
Loris <monestierloris11@gmail.com>
2023-01-29 19:46:22 +0100

    itopy

--- src/i_to_py/intermediate_to_py.py
+++ src/i_to_py/intermediate_to_py.py
@@ -12,9 +12,22 @@ class Intermediatetopy:
     def intermediate_to_py(self) -> str:
         for instruction in self.inter.instructions:
             if isinstance(instruction, Ilib.Assign):
-                self.add_to_code(f"{instruction.name} = {instruction.value.value}")
-        
+                var_assign:str = ""
+                var_assign += f"{instruction.name}:{get_type_as_str(instruction.value.type)} = "
+                var_assign += "\"" if instruction.value.type == Ilib.TYPES.STRING else ''
+                var_assign += instruction.value.value
+                var_assign += "\"" if instruction.value.type == Ilib.TYPES.STRING else ""
+                self.add_to_code(var_assign)
         return self.output
         
     def add_to_code(self, string: str):
         self.output += self.indentation*self.indent + string + "\n"
+        
+def get_type_as_str(type: Ilib.TYPES) -> str:
+    return {
+        Ilib.TYPES.INT: "int",
+        Ilib.TYPES.FLOAT: "float",
+        Ilib.TYPES.STRING: "str",
+        Ilib.TYPES.BOOL: "bool",
+        Ilib.TYPES.TYPE: "type",
+    }.get(type, "type")
